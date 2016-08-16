4263c3a021f5cc190371edd3e5b38a61bfdf0944
Fellipe Caetano <fellipe.caetano4@gmail.com>
2016-08-16 17:39:59 -0300

    Support for NIB-based object loading

--- IBentifiers/Classes/Reuse/NibLoadable.swift
+++ IBentifiers/Classes/Reuse/NibLoadable.swift
@@ -18,4 +18,14 @@ public extension NibLoadable {
     static var nib: UINib {
         return UINib(nibName: self.name, bundle: self.bundle)
     }
+
+    static func loadFirst(owner owner: AnyObject? = nil, options: [NSObject: AnyObject]? = nil) -> Self {
+        guard let fromNib = nib.instantiateWithOwner(owner, options: options).first else {
+            fatalError("The .nib named \(name) is empty")
+        }
+        guard let typedObject = fromNib as? Self else {
+            fatalError("Expected first object of .nib named \(name) to be of type \(self) but actual type is \(fromNib.self)")
+        }
+        return typedObject
+    }
 }
